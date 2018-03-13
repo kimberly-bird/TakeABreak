0d517e800d00075d9429fab80d74519e25acdeab
KimberlyJBird <kimberly.j.bird@gmail.com>
2018-03-13 10:48:26 -0500

    dashboard now displays user specific breaks

--- TakeABreak/ViewComponents/DayDetailViewComponent.cs
+++ TakeABreak/ViewComponents/DayDetailViewComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TakeABreak.Data;
@@ -12,12 +13,17 @@ namespace TakeABreak.ViewComponents
     public class DayDetailViewComponent : ViewComponent
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public DayDetailViewComponent(ApplicationDbContext context)
+        public DayDetailViewComponent(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
-        
+
+        // This task retrieves the currently authenticated user
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var items = await GetItemsAsync();
@@ -26,7 +32,10 @@ namespace TakeABreak.ViewComponents
         
         private async Task<Day> GetItemsAsync()
         {
+            ApplicationUser user = await GetCurrentUserAsync();
+
             var day = await _context.Day
+                .Where(u => u.User == user)
                 .OrderByDescending(d => d.Date)
                 .FirstAsync();
 
